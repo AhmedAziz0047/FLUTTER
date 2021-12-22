c5db038cad3407bce4f2367f17f19daa04e92212
bilel713 <bilelbenaissa06@gmail.com>
2021-12-22 16:42:29 +0100

    login-page/regestration-page/theme-helper/header-widgets

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutterproject/admin/adminFlights.dart';
 import 'package:flutterproject/admin/bottomnavbar.dart';
 import 'package:flutterproject/widgets/vols_item.dart';
+import 'package:flutterproject/pages/login_page.dart';
 
 class HomeScreen extends StatefulWidget {
   int data;
-  HomeScreen({ Key? key, required this.data}) : super(key: key);
+  HomeScreen({Key? key, required this.data}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -14,26 +15,26 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(  
-      child:Scaffold(
-        appBar: AppBar(
-          title:Text("Our Trips"),
-          backgroundColor: Colors.blue[900],
-        ),
-        backgroundColor: Colors.white,
-        body:bottomnavbar()
-        // SingleChildScrollView(child:Wrap(children: List<Widget>.generate(5, (index) {return adminFlights();}))),
-      )
-      // body: Container(
-      //     decoration: BoxDecoration(
-      //     image: DecorationImage(
-      //       image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRngnXL9xDDjxMzGZO9CWXhKpioEu0nxpZJww&usqp=CAU"),
-      //       fit: BoxFit.cover,
-      //     ),
-      //   ),
-      //   child: null /* add child content here */,
-      //   ),
-      
-    );
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text("Our Trips"),
+              backgroundColor: Colors.blue[900],
+            ),
+            backgroundColor: Colors.white,
+            body: LoginPage()
+            // SingleChildScrollView(child:Wrap(children: List<Widget>.generate(5, (index) {return adminFlights();}))),
+            )
+        // body: Container(
+        //     decoration: BoxDecoration(
+        //     image: DecorationImage(
+        //       image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRngnXL9xDDjxMzGZO9CWXhKpioEu0nxpZJww&usqp=CAU"),
+        //       fit: BoxFit.cover,
+        //     ),
+        //   ),
+        //   child: null /* add child content here */,
+        //   ),
+
+        );
   }
 }
