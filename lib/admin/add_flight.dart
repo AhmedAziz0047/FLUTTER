4036a9aa42b169675ba675bf246574b82d65a1a8
AhmedAziz0047 <ahmedazizsouissi@gmail.com>
2021-12-28 16:44:51 +0100

    improving admin flights design

---
+++ lib/admin/add_flight.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class addFlight extends StatefulWidget {
+  const addFlight({ Key? key }) : super(key: key);
+
+  @override
+  _addFlightState createState() => _addFlightState();
+}
+
+class _addFlightState extends State<addFlight> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
