9687add786215fb97f4813aadd3bacfd3f02bd9e
bilel713 <bilelbenaissa06@gmail.com>
2021-12-31 16:40:53 +0100

    user routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,5 +7,10 @@ const auth = require('./../middlewares/auth');
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
 router.get('/profile/:id', auth, userController.profile);
+router.get('/api/getUsers',getUsers);
+router.get('/api/getUserById/:id',getUserById);
+router.post('/api/addUser',createUser);
+router.delete('/api/deleteUser/:id',deleteUser);
+router.put('/api/editUser/:id', updateUSer);
 
 module.exports = router;
