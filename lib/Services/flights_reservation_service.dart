aa8cd9d0a4051ff5798605122c0b9133737daf51
bilel713 <bilelbenaissa06@gmail.com>
2021-12-31 17:11:47 +0100

    user service

--- lib/Services/flights_reservation_service.dart
+++ lib/Services/flights_reservation_service.dart
@@ -3,15 +3,19 @@ import 'dart:math';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterproject/admin/add_user.dart';
 import 'package:flutterproject/admin/admin_flights.dart';
 import 'package:flutterproject/admin/admin_reservations.dart';
 import 'package:flutterproject/admin/appbar_bottomnavbar.dart';
 import 'package:flutterproject/pages/acceuil_flight.dart';
+import 'package:flutterproject/pages/login_page.dart';
 import 'package:flutterproject/widgets/mytoast.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class Flights_reservation_service {
+  get id => null;
+
   /////////////////////notification////////
   notify(String nom, String pren, String confnumber) async {
     await AwesomeNotifications().createNotification(
@@ -201,6 +205,96 @@ class Flights_reservation_service {
   Future deleteRes(String id) async {
     final deleteurl =
         (Uri.parse('http://192.168.1.71:3000/api/deleteReservations/$id'));
+    final response = await http.delete(deleteurl);
+    print(response.statusCode);
+    print(response.body);
+    return json.decode(response.body);
+  }
+  ////////////////reservations//////////////////////
+
+  // User admin
+  Future AddUserAPI(context, String name, String email, String password) async {
+    var url = 'http://192.168.1.71:3000/api/auth/api/addUser';
+    final res = await http.post(Uri.parse(url),
+        body: {'name': name, 'email': email, 'password': password});
+    print(res.statusCode);
+    if (res.statusCode == 201) {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const AppbarBottomnavbar()));
+    } else {
+      MyToast().showtoast("Check your informations");
+    }
+    return json.decode(res.body);
+  }
+
+//addUserClient
+  Future addUSerClient(
+      context, String name, String email, String password) async {
+    var url = 'http://192.168.1.71:3000/api/auth/api/addUser';
+    final res = await http.post(Uri.parse(url),
+        body: {'name': name, 'email': email, 'password': password});
+    if (res.statusCode == 200) {
+      String ch = "0123456789";
+      String confN = "";
+      for (int i = 0; i < 30; i++) {
+        confN += ch[Random().nextInt(10)];
+      }
+      Flights_reservation_service().notify(name, email, password);
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const LoginPage()));
+    } else {
+      MyToast().showtoast("Check your informations");
+    }
+
+    // print(res.statusCode);
+    return json.decode(res.body);
+  }
+
+  //getUsers
+  Future<List> getUSer() async {
+    try {
+      print("api works");
+      var res = await http
+          .get(Uri.parse('http://192.168.1.71:3000/api/auth/api/editUser'));
+      var decodedData = jsonDecode(res.body);
+      print(decodedData);
+      print("works");
+      return decodedData;
+    } catch (SocketException) {
+      return Future.error("api not works");
+    }
+  }
+
+//updateUser
+  Future updateUseradmin(
+      context, String name, String email, String password) async {
+    final url = Uri.parse('http://192.168.1.71:3000/api/auth/api/editUser/$id');
+    final res = await http
+        .put(url, body: {'name': name, 'email': email, 'password': password});
+    if (res.statusCode == 200) {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const AppbarBottomnavbar()));
+    } else {
+      MyToast().showtoast("Please your infos");
+    }
+    // print (res.statusCode);
+    return json.decode(res.body);
+  }
+
+//get userreservationbyid
+  Future<Map> getOneUser(String id) async {
+    final deleteurl =
+        (Uri.parse('http://192.168.1.71:3000/api/getUserbyid/$id'));
+    final response = await http.get(deleteurl);
+    // print(response.statusCode);
+    // print(response.body);
+    return json.decode(response.body);
+  }
+
+  //deleteUser
+  Future deleteUser(String id) async {
+    final deleteurl =
+        (Uri.parse('http://192.168.1.71:3000/api/auth/api/deleteUser/$id'));
     final response = await http.delete(deleteurl);
     print(response.statusCode);
     print(response.body);
